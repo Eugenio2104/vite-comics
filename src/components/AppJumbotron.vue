b81f9054cd4dd521634847593832ee40c848eb76
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 17:12:48 +0100

    aggiunta cartella jumbo

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "AppJumbotron",
+};
+</script>
+
+
+<template>
+  <div class="container-jumbo">
+    <span> content goes here </span>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+height: 50px;
+</style>
