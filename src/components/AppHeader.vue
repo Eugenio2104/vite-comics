75f25a008264682a3a90f8e35f5ccf50c7c3fbaa
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 16:10:48 +0100

    repo rinominata

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,9 +6,33 @@ export default {
 
 
 <template>
-  <header>H</header>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="/dc-logo.png" alt="" />
+      </div>
+      <nav>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </nav>
+    </div>
+  </header>
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  ul {
+  }
+}
 </style>
