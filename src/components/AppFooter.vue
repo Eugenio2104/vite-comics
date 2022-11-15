68398452e4e5ddceb889304d4d56fb35a1f2254a
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 19:55:43 +0100

    inserimento footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,9 +6,116 @@ export default {
 
 
 <template>
-  <footer>F</footer>
+  <footer>
+    <div class="container">
+      <div class="list-footer">
+        <nav>
+          <h3>dc comics</h3>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">news</a>
+            </li>
+          </ul>
+          <h3>shop</h3>
+          <ul>
+            <li>
+              <a href="#">Shop Dc</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="list-footer">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Terms Of Use</a>
+            </li>
+            <li>
+              <a href="#">Privacy policy(New)</a>
+            </li>
+            <li>
+              <a href="#">Ad Choices</a>
+            </li>
+            <li>
+              <a href="#">Adverstising</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Subscription</a>
+            </li>
+            <li>
+              <a href="#">Talent Workshop</a>
+            </li>
+            <li>
+              <a href="#">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="#">Ratings</a>
+            </li>
+            <li>
+              <a href="#">Shop Help</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="list-footer">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">DC</a>
+            </li>
+            <li>
+              <a href="#">MAD Magazine</a>
+            </li>
+            <li>
+              <a href="#">DC Kids</a>
+            </li>
+            <li>
+              <a href="#">DC Universe</a>
+            </li>
+            <li>
+              <a href="#">DC Power Visa</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </footer>
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+
+footer {
+  width: 100%;
+  background-color: black;
+}
 </style>
