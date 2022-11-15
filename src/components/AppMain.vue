5b515e1a13379c820b1d6e8d75a721cc4fa86e84
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 17:37:12 +0100

    finito main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,26 +7,28 @@ export default {
 
 <template>
   <main>
-    <div class="container">
-      <div class="box">
-        <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-        <span> DIGITAL COMICS </span>
-      </div>
-      <div class="box">
-        <img src="../assets/img/buy-comics-merchandise.png" alt="" />
-        <span> DC Merchandise </span>
-      </div>
-      <div class="box">
-        <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
-        <span> subscription </span>
-      </div>
-      <div class="box">
-        <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
-        <span> comic shop locator </span>
-      </div>
-      <div class="box">
-        <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
-        <span> dc power visa </span>
+    <div class="container-main">
+      <div class="container">
+        <div class="box">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+          <span> DIGITAL COMICS </span>
+        </div>
+        <div class="box">
+          <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+          <span> DC Merchandise </span>
+        </div>
+        <div class="box">
+          <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+          <span> subscription </span>
+        </div>
+        <div class="box">
+          <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+          <span> comic shop locator </span>
+        </div>
+        <div class="box">
+          <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+          <span> dc power visa </span>
+        </div>
       </div>
     </div>
   </main>
@@ -36,8 +38,23 @@ export default {
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixin" as *;
-
-.container {
+.container-main {
+  width: 100%;
   background-color: $primary-color;
+  .container {
+    height: 200px;
+    @include centralFlex("horizontal");
+    justify-content: space-around;
+    .box {
+      color: white;
+      @include centralFlex("vertical");
+      img {
+        width: 50px;
+      }
+      span {
+        text-transform: uppercase;
+      }
+    }
+  }
 }
 </style>
