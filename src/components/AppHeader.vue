b92a140c94b0331bc17fddc25d57820d97b78ecc
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 17:07:36 +0100

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,16 +12,18 @@ export default {
         <img src="/dc-logo.png" alt="" />
       </div>
       <nav>
-        <li><a href="#">CHARACTERS</a></li>
-        <li><a href="#">COMICS</a></li>
-        <li><a href="#">MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOP</a></li>
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a class="active" href="#">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
       </nav>
     </div>
   </header>
@@ -29,10 +31,37 @@ export default {
 
 
 <style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
 .container {
-  display: flex;
-  justify-content: space-between;
-  ul {
+  height: 100px;
+  @include centralFlex("vertical");
+  @include centralFlex("space-between");
+  .logo {
+    img {
+      width: 60px;
+    }
+  }
+  nav {
+    height: 100%;
+    @include centralFlex;
+    ul {
+      @include centralFlex("horizzontal");
+      li {
+        list-style: none;
+        padding-left: 20px;
+        a {
+          font-weight: 600;
+          font-size: 0.9rem;
+          color: #464646;
+          text-decoration: none;
+          &.active,
+          &:hover {
+            color: $primary-color;
+          }
+        }
+      }
+    }
   }
 }
 </style>
