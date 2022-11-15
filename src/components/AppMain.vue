f29870abc971bdceecbe23ce75cdf18960cc39cb
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-15 17:30:57 +0100

    inizio struttura main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,9 +6,38 @@ export default {
 
 
 <template>
-  <main>M</main>
+  <main>
+    <div class="container">
+      <div class="box">
+        <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+        <span> DIGITAL COMICS </span>
+      </div>
+      <div class="box">
+        <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+        <span> DC Merchandise </span>
+      </div>
+      <div class="box">
+        <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+        <span> subscription </span>
+      </div>
+      <div class="box">
+        <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+        <span> comic shop locator </span>
+      </div>
+      <div class="box">
+        <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+        <span> dc power visa </span>
+      </div>
+    </div>
+  </main>
 </template>
 
 
-<style>
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+
+.container {
+  background-color: $primary-color;
+}
 </style>
